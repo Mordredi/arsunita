4a1d79836885f6282103e461076d773e3cec9d14
Arthur <arthur.p.wright@gmail.com>
2015-05-19 15:22:29 -0400

    Added Follower model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519183009) do
+ActiveRecord::Schema.define(version: 20150519192053) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20150519183009) do
 
   add_index "events", ["venue_id"], name: "index_events_on_venue_id"
 
+  create_table "followers", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followee_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "friends", force: :cascade do |t|
     t.integer  "user_id"
     t.boolean  "confirmed"
@@ -34,6 +41,14 @@ ActiveRecord::Schema.define(version: 20150519183009) do
   end
 
   add_index "friends", ["user_id"], name: "index_friends_on_user_id"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "friendee_id"
+    t.integer  "friender_id"
+    t.boolean  "confirmed"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "shows", force: :cascade do |t|
     t.datetime "date"
---
+++ spec/factories/followers.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :follower do
+    follower_id 1
+followee_id 1
+  end
+
+end
