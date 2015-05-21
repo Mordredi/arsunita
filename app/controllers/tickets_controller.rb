b32236da6922caca2f06d81eff967874ee91855d
Arthur <arthur.p.wright@gmail.com>
2015-05-20 21:41:49 -0400

    Can buy tickets to events. Tickets shown on User page

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,19 +1,18 @@
 class TicketsController < ApplicationController
 
   def create
-    @event = Event.find(params[:id])
-    @ticket = @event.tickets.build(ticket_params)
-    @ticket.user_id = current_user
+    @ticket = current_user.tickets.build(ticket_params)
     if @ticket.save
-      redirect_to event_url(@event), :notice => 'Tickets were purchased'
+      # redirect_to event_url(@event), :notice => 'Tickets were purchased'
+      redirect_to user_url(current_user)
     else
-      redirect_to event_url(@event), :alert => 'Error purchasing tickets'
+      # redirect_to event_url(@event), :alert => 'Error purchasing tickets'
     end
   end
 
   private
   def ticket_params
-    params.require(:tickets).permit(:num_of_general)
+    params.require(:ticket).permit(:num_of_general, :show_id)
   end
 
 end
